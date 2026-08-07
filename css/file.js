78ea6d4589a69789b11ba027f091d2587a764088
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2026-08-07 13:53:31 +0530

    changed

--- css/file.js
+++ css/file.js
@@ -1,22 +1,10 @@
-setInterval(mufun, 1000);
-function mufun() {
-  let b1 = document.getElementById("on");
-  let b2 = document.getElementById("off");
-  let view = document.getElementById("menu");
-
-  b1.addEventListener("click", function () {
-    if ((view.style.display = "none")) {
-      view.style.display = "block";
-
-    }
-  })
-
-  b2.addEventListener("click", function () {
-    if ((view.style.display = "block")) {
-      view.style.display = "none";
-    }
-  })
-}
+const menu = document.getElementById("menu");
+document.getElementById("on").addEventListener("click", () => {
+  menu.style.display = "block";
+});
+document.getElementById("off").addEventListener("click", () => {
+  menu.style.display = "none";
+});
 
 const currentPath = window.location.pathname;
 const navLinks = document.querySelectorAll("nav ul li a");
